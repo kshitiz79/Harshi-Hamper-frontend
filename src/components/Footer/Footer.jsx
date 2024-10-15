9649a96d9f166f0fd1d9fed24ef6f9c1e8a63426
kshitiz79 <kshitiz.21b0131170@abes.ac.in>
2024-10-16 00:28:09 +0530

    Your commit message

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,66 +2,6 @@ import React from "react";
 import "./Footer.scss";
 import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
 import Payment from "../../assets/payments.png";
-const Footer = () => {
-    return (
-        <div className="footer">
-            <div className="footer-content">
-                <div className="col">
-                    <div className="title">About</div>
-                    <div className="text">
-                        Voluptatem accusantium doloremque laudantium, totam rem
-                        aperiam, eaque ipsa quae ab illo inventore veritatis et
-                        quasi architecto beatae vitae dicta sunt explicabo eaque
-                        ipsa quae ab illo.
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="title">Contact</div>
-                    <div className="c-item">
-                        <FaLocationArrow />
-                        <div className="text">
-                            T.M.C Near Black Stone ABES Engineering Collage
-                        </div>
-                    </div>
-                    <div className="c-item">
-                        <FaMobileAlt />
-                        <div className="text">Phone: 8127520552</div>
-                    </div>
-                    <div className="c-item">
-                        <FaEnvelope />
-                        <div className="text">Email: harshitatyagi2@gmail.com</div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="title">Categories</div>
-                    <span className="text">Hampers</span>
-                    <span className="text">Customize Comics</span>
-                    <span className="text">Neon Light</span>
-                    <span className="text">Frames</span>
-                    <span className="text">Standee</span>
-                    <span className="text">Soft Toys</span>
-                
-                </div>
-                <div className="col">
-                    <div className="title">Pages</div>
-                    <span className="text">Home</span>
-                    <span className="text">About</span>
-                    <span className="text">Privacy Policy</span>
-                    <span className="text">Returns</span>
-                    <span className="text">Terms & Conditions</span>
-                    <span className="text">Contact Us</span>
-                </div>
-            </div>
-            <div className="bottom-bar">
-                <div className="bottom-bar-content">
-                    <span className="text">
-                        Harshi's Hamper  Created Harshita Tyagi. For Providing 
-                        Customized Gifts.
-                    </span>
-                    <img src={Payment} import React from "react";
-import "./Footer.scss";
-import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
-import Payment from "../../assets/payments.png";
 
 const Footer = () => {
     return (
@@ -81,7 +21,7 @@ const Footer = () => {
                     <div className="c-item">
                         <FaLocationArrow />
                         <div className="text">
-                            T.M.C Near Black Stone ABES Engineering Collage
+                            T.M.C Near Black Stone ABES Engineering College
                         </div>
                     </div>
                     <div className="c-item">
@@ -115,18 +55,9 @@ const Footer = () => {
             <div className="bottom-bar">
                 <div className="bottom-bar-content">
                     <span className="text">
-                        Harshi's Hamper Created Harshita Tyagi. For Providing
-                        Customized Gifts.
+                        Harshi's Hamper - Created by Harshita Tyagi. Providing Customized Gifts.
                     </span>
                     <img src={Payment} alt="Payment Methods" />
-                </div>
-            </div>
-        </div>
-    );
-};
-
-export default Footer;
-/>
                 </div>
             </div>
         </div>
